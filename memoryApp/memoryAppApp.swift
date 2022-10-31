2b279078768e09a6e3abee7b432e089f5575144b
Alejandro <aaurzola@gmail.com>
2022-10-31 18:12:48 +0100

    Initial Commit

---
+++ memoryApp/memoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryAppApp.swift
+//  memoryApp
+//
+//  Created by Alejandro Urzola on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct memoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
